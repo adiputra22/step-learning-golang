44e61c468cd7773f12bed49f1bd04054c8af15a2
adiputra-plankton <tuts.adiputra@gmail.com>
2020-08-21 11:23:33 +0700

    learning looping in map

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
-	fmt.Println("Hai golang, apa kabar?")
+	grades := map[string]float64{"Alma": 0, "Rohit": 86.5, "Adi": 62}
+
+	var names []string
+
+	for name := range grades {
+		names = append(names, name)
+	}
+
+	sort.Strings(names)
+
+	for _, name := range names {
+		fmt.Printf("%s punya grade sebesar %0.2f%% \n", name, grades[name])
+	}
 }
