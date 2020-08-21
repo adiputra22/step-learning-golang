7b277a50a01db199976e2f1fc90ae53b56178c59
adiputra-plankton <tuts.adiputra@gmail.com>
2020-08-21 11:04:33 +0700

    learn access non existing key in map

--- main.go
+++ main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hai golang, apa kabar?")
+	status("Alma")
+	status("Adi")
+}
+
+func status(name string) {
+	grades := map[string]float64{"Alma": 0, "Rohit": 86.5}
+
+	grade, ok := grades[name]
+
+	if !ok {
+		fmt.Printf("nama %s tidak ada di record!\n", name)
+	} else {
+		if grade < 60 {
+			fmt.Printf("%s is failing!\n", name)
+		}
+	}
 }
