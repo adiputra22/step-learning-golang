d434cf8dd2f66402ce0d019dff27a6ff66f6cf19
adiputra-plankton <tuts.adiputra@gmail.com>
2020-08-22 10:18:59 +0700

    struct as parameter

--- main.go
+++ main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+type part struct {
+	description string
+	count       int
+}
+
 func main() {
-	fmt.Println("Hai golang, apa kabar?")
+	var bolts part
+
+	bolts.description = "ini bolts description"
+	bolts.count = 1243
+
+	showInfo(bolts)
+}
+
+func showInfo(p part) {
+	fmt.Println("Description bolts:", p.description)
+	fmt.Println("Count bolts:", p.count)
 }
