ee6ceac4a2ee1822f6450e9cbedbc85bdb81fc1f
adiputra-plankton <tuts.adiputra@gmail.com>
2020-08-22 10:13:38 +0700

    use struct as type

--- main.go
+++ main.go
@@ -2,6 +2,28 @@ package main
 
 import "fmt"
 
+type part struct {
+	description string
+	count       int
+}
+
+type car struct {
+	name     string
+	topSpeed int
+}
+
 func main() {
-	fmt.Println("Hai golang, apa kabar?")
+	var porsche car
+	porsche.name = "Porsche type AAA"
+	porsche.topSpeed = 325
+
+	fmt.Println("Name:", porsche.name)
+	fmt.Println("Top speed:", porsche.topSpeed)
+
+	var bolts part
+	bolts.description = "Hex bolt"
+	bolts.count = 24
+
+	fmt.Println("Bolt description:", bolts.description)
+	fmt.Println("Bolt Count:", bolts.count)
 }
