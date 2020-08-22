eb8309a7e5459e251742e5f4b742997835b49ec9
adiputra-plankton <tuts.adiputra@gmail.com>
2020-08-22 13:44:31 +0700

    passing struct using pointers

--- main.go
+++ main.go
@@ -2,6 +2,39 @@ package main
 
 import "fmt"
 
+type subscriber struct {
+	name   string
+	rate   float64
+	active bool
+}
+
+func printInfo(s *subscriber) {
+	fmt.Println("Name:", s.name)
+	fmt.Println("Monthly Rate:", s.rate)
+	fmt.Println("Active?:", s.active)
+}
+
+func defaultSubscriber(name string) *subscriber {
+	var s subscriber
+
+	s.name = name
+	s.rate = 5.99
+	s.active = true
+
+	return &s
+}
+
+func applyDiscount(s *subscriber) {
+	s.rate = 4.99
+}
+
 func main() {
-	fmt.Println("Hai golang, apa kabar?")
+	subscriber1 := defaultSubscriber("Adiputra")
+
+	applyDiscount(subscriber1)
+
+	printInfo(subscriber1)
+
+	subscriber2 := defaultSubscriber("Irsyad")
+	printInfo(subscriber2)
 }
