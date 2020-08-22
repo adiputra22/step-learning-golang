27c641cd72c7322728245675c476312c863062b8
adiputra-plankton <tuts.adiputra@gmail.com>
2020-08-22 10:02:14 +0700

    init struct

--- main.go
+++ main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hai golang, apa kabar?")
+	var myStruct struct {
+		number float64
+		word   string
+		toogle bool
+	}
+
+	myStruct.number = 25.2
+	myStruct.word = "hai struct"
+	myStruct.toogle = true
+
+	fmt.Printf("%#v\n", myStruct)
+
+	fmt.Println(myStruct.number)
 }
