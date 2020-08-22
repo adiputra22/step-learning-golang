84485d5ca2dd3655e71f208a1cd2c65a4c4dff8b
adiputra-plankton <tuts.adiputra@gmail.com>
2020-08-22 10:24:27 +0700

    struct as returned function

--- main.go
+++ main.go
@@ -19,4 +19,16 @@ func main() {
 func showInfo(p part) {
 	fmt.Println("Description bolts:", p.description)
 	fmt.Println("Count bolts:", p.count)
+
+	newPart := setPartByDescription("bolt 123")
+	fmt.Println("Description new part:", newPart.description)
+	fmt.Println("Count new part:", newPart.count)
+}
+
+func setPartByDescription(description string) part {
+	var p part
+	p.description = description
+	p.count = 5
+
+	return p
 }
