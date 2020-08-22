a6d8ca22602dabccf3ce2dd95769d4fb4de7efe0
adiputra-plankton <tuts.adiputra@gmail.com>
2020-08-22 11:23:57 +0700

    variable pointers access struct

--- main.go
+++ main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+type myStruct struct {
+	myField int
+}
+
 func main() {
-	fmt.Println("Hai golang, apa kabar?")
+	var value myStruct
+	value.myField = 3
+
+	var pointer *myStruct = &value
+
+	pointer.myField = 8
+
+	// fmt.Println(*pointer.myField) can not use this
+
+	// fmt.Println((*pointer).myField) we can use this code to call pointer
+
+	fmt.Println(pointer.myField) // or dot operator (as default) provide us to access field via pointer
+
+	fmt.Println(value.myField)
 }
